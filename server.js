2f41b2a4f58c828201c42d8dffcfe4eb87c62963
Jackson Yang <jacksony@gmail.com>
2019-12-13 22:39:24 +0800

    serve- static 套件加入

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 //import 套件
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 //把html傳給使用者
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/toDoList.html');
 })
+
+app.use(serveStatic(path.join(__dirname, 'public')))
 
 app.use("*", function (req, res) {
     res.status(404).send("404");
